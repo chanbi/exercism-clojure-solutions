7d21b3945845d717a6332ca59da8b92727b17367
Chantal Bisson <chantal.bisson@msss.gouv.qc.ca>
2018-09-20 22:25:07 -0400

    solve armstrong-number exercise

---
+++ armstrong-numbers/src/armstrong_numbers.clj
@@ -0,0 +1,11 @@
+(ns armstrong-numbers)
+
+(defn power [num pow] 
+  (apply * (repeat pow num)))
+
+(defn digits [num]
+  (rseq (mapv #(mod % 10) (take-while pos? (iterate #(quot % 10) num)))))
+
+(defn armstrong? [num] 
+  (def dgts (digits num))
+  (= num (apply + (map #(power % (count dgts)) dgts))))
