e0eff8cbf9eb3a1eac100c3fc014bc2e01fcb05c
Chantal Bisson <chantal.bisson@msss.gouv.qc.ca>
2018-10-18 00:12:08 -0400

    solve binary exersice v2

--- binary/src/binary.clj
+++ binary/src/binary.clj
@@ -1,15 +1,11 @@
 (ns binary)
 
-(defn binary-power [pos digit] 
-  (apply * digit (repeat pos 2)))
+(defn bin-digits [string] 
+  (map #({\0 0 \1 1} % 0) string))
 
-(defn number-else-zero [num] 
-  (if (number? num) num 0))
+(defn bin-power [index digit] 
+  {:pre [(number? digit)]}
+  (apply * digit (repeat index 2)))
 
-(defn digits [string] 
-    (map (comp number-else-zero read-string str) string))
-
-; (read-string (str "2r" binary))
-(defn to-decimal [binary]
-  (apply + (map-indexed binary-power (reverse (digits binary)))))
-  
+(defn to-decimal [string]
+  (apply + (map-indexed bin-power (reverse (bin-digits string)))))
