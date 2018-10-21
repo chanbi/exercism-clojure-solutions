6a0b5e0fc9fddc0ef7f69667669cc305b68b78ab
Chantal Bisson <chantal.bisson@msss.gouv.qc.ca>
2018-10-21 19:51:01 -0400

    solve collatz-conjecture exercise

--- collatz-conjecture/src/collatz_conjecture.clj
+++ collatz-conjecture/src/collatz_conjecture.clj
@@ -1,5 +1,7 @@
 (ns collatz-conjecture)
 
-(defn collatz [num]
-  ;; your code goes here
-)
+(defn collatz [number]
+  {:pre [(> number 0)]}
+  (loop [count 0 num number]
+    (if (= 1 num) count 
+      (recur (inc count) (if (even? num) (/ num 2) (+ (* num 3) 1))))))  
