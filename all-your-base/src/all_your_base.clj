b4974bca9cde200da1aa9840362f839812340421
Chantal Bisson <chantal.bisson@msss.gouv.qc.ca>
2018-10-20 21:40:24 -0400

    solve all-your-base exercise

--- all-your-base/src/all_your_base.clj
+++ all-your-base/src/all_your_base.clj
@@ -1,5 +1,24 @@
 (ns all-your-base)
 
-(defn convert [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn to-base [number base]
+  (if (nil? number) '()
+    (if (zero? number) '(0)
+      (loop [digits '() num number]
+        (if (zero? num) digits
+          (recur (conj digits (rem num base))
+                (quot num base)))))))
+
+(defn to-decimal [digits base]
+  (if (empty? digits) nil 
+    (apply + (map-indexed #(apply * %2 (repeat %1 base)) (reverse digits)))))
+
+(defn valid-digits? [digits base]
+  (every? #(and (not (neg? %)) (< % base)) digits))
+
+(defn- convert-impl [in-base digits out-base] 
+  {:pre  [(> in-base 1) (> out-base 1) (valid-digits? digits in-base)]
+   :post [(valid-digits? % out-base)]}
+  (to-base (to-decimal digits in-base) out-base))
+
+(defn convert [in-base digits out-base] 
+  (try (convert-impl in-base digits out-base) (catch AssertionError e nil)))
