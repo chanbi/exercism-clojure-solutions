955dfdbb6d13b7f854922621403e82718cc3d406
Chantal Bisson <chantal.bisson@msss.gouv.qc.ca>
2018-10-21 22:12:15 -0400

    solve binary exercise v4

--- binary/src/binary.clj
+++ binary/src/binary.clj
@@ -1,7 +1,8 @@
 (ns binary)
 
-(defn bin-digits [string] 
-  (map #({\0 0 \1 1} % 0) string)) 
+(defn- to-digits [string] 
+  (let [bin-dgts {\0 0 \1 1}]
+    (when (every? bin-dgts string) (map #(bin-dgts %) string))))
 
 (defn to-decimal [string]
-  (reduce #(+ (* 2 %1) %2) 0 (bin-digits string)))
+  (reduce #(+ (* 2 %1) %2) 0 (to-digits string)))
