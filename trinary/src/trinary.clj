0970329dbbe3d9823e98b3507ffa2ec87917dcfa
Chantal Bisson <chantal.bisson@msss.gouv.qc.ca>
2018-10-21 22:08:59 -0400

    solve trinary exercise v2

--- trinary/src/trinary.clj
+++ trinary/src/trinary.clj
@@ -2,7 +2,7 @@
 
 (defn- to-digits [string] 
   (let [tri-dgts {\0 0 \1 1 \2 2}]
-    (when (every? #(contains? tri-dgts %) string) (map #(tri-dgts %) string))))
+    (when (every? tri-dgts string) (map #(tri-dgts %) string))))
   
 (defn to-decimal [string]
   (reduce #(+ (* 3 %1) %2) 0 (to-digits string)))
