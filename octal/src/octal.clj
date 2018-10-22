4c3e733703b695163a56e5c833047b651f481156
Chantal Bisson <chantal.bisson@msss.gouv.qc.ca>
2018-10-21 22:24:43 -0400

    solve octal exercise

--- octal/src/octal.clj
+++ octal/src/octal.clj
@@ -1,5 +1,9 @@
 (ns octal)
 
-(defn to-decimal [] ;; <- arglist goes here
-  ;; your code goes here
-  )
+(defn- to-digits [string] 
+  (let [oct-dgts {\0 0 \1 1 \2 2 \3 3 \4 4 \5 5 \6 6 \7 7}]
+    (when (every? oct-dgts string) (map #(oct-dgts %) string))))
+
+(defn to-decimal [string] 
+  (reduce #(+ (* 8 %1) %2) 0 (to-digits string))) 
+  
