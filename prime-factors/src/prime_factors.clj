b64a69f3d72e9f05dc65f58de39f22e6cddb36b0
Chantal Bisson <2739403+chanbi@users.noreply.github.com>
2019-09-19 23:16:52 -0400

    solve prime-factors exercise

--- prime-factors/src/prime_factors.clj
+++ prime-factors/src/prime_factors.clj
@@ -1,5 +1,11 @@
 (ns prime-factors)
 
-(defn of [] ;; <- arglist goes here
-      ;; your code goes here
-      )
+(defn of [number]
+  {:pre [(number? number)]
+   post [(seq? %) (= (reduce * %) number)]}
+  (loop [divisor 2 result number prime-factors []]
+    (if (< result 2)
+      prime-factors
+      (if (zero? (rem result divisor))
+        (recur divisor (/ result divisor) (conj prime-factors divisor))
+        (recur (inc divisor) result prime-factors)))))
