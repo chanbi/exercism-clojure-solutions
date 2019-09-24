d776b9d52371a8c8217d6238a3a0a6197386bd94
Chantal Bisson <2739403+chanbi@users.noreply.github.com>
2019-09-24 13:31:20 -0400

    solve sum-of-multiples exercise

--- sum-of-multiples/src/sum_of_multiples.clj
+++ sum-of-multiples/src/sum_of_multiples.clj
@@ -1,5 +1,6 @@
 (ns sum-of-multiples)
 
-(defn sum-of-multiples [] ;; <- arglist goes here
-      ;; your code goes here
-      )
+(defn sum-of-multiples [factors number]
+  {:pre [(seq? factors) (number? number)]
+   :post [(number? %)]}
+  (reduce + (distinct (mapcat #(range 0 number %) factors))))
