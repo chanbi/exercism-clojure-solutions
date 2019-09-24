d4c9d0a46a675ae9a962cd2cf0e295012c7a4af4
Chantal Bisson <2739403+chanbi@users.noreply.github.com>
2019-09-24 16:16:22 -0400

    solve nth-prime exersice

--- nth-prime/src/nth_prime.clj
+++ nth-prime/src/nth_prime.clj
@@ -1,5 +1,13 @@
 (ns nth-prime)
 
-(defn nth-prime [] ;; <- arglist goes here
-  ;; your code goes here
-  )
+(defn- prime? [number]
+  (if (< number 2)
+    false
+    (not-any? #(zero? (rem number %)) (range 2 number))))
+
+(defn nth-prime [number]
+  {:pre [(number? number)]
+   post [(number? %)]}
+  (if (pos? number)
+    (last (take number (filter prime? (range))))
+    (throw (IllegalArgumentException.))))
