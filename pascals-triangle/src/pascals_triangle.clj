dda3e9b388e6a402c457d66b465d5e14cbfe4e32
Chantal Bisson <2739403+chanbi@users.noreply.github.com>
2019-09-24 23:53:30 -0400

    solve pascals-triangle exercise v2

--- pascals-triangle/src/pascals_triangle.clj
+++ pascals-triangle/src/pascals_triangle.clj
@@ -1,12 +1,8 @@
 (ns pascals-triangle)
 
-(def triangle
-  ((fn build [row]
-     (let [next-row (concat [1] (map + row (rest row)) [1])]
-       (lazy-seq (cons row (build next-row)))))
-   '(1)))
+(def triangle (iterate #(lazy-cat [1] (mapv + % (rest %)) [1]) '(1)))
 
 (defn row [number]
   {:pre [(number? number)]
-   :post [(sequential? %) (= (reduce + %) (int (Math/pow 2 (dec number))))]}
+   :post [(seq? %) (= (reduce + %) (int (Math/pow 2 (dec number))))]}
   (nth triangle (dec number)))
