6efcf2a4143817062d2c250a0424dbcc234bc107
Chantal Bisson <2739403+chanbi@users.noreply.github.com>
2019-09-30 22:13:33 -0400

    solve spiral-matrix exercise

--- spiral-matrix/src/spiral_matrix.clj
+++ spiral-matrix/src/spiral_matrix.clj
@@ -1,5 +1,16 @@
 (ns spiral-matrix)
 
-(defn spiral [n] ;; <- arglist goes here
-  ;; your code goes here
-  )
+(defn transpose [matrix]
+  (apply map list matrix))
+
+(defn rotate [matrix]
+  (map reverse (transpose matrix)))
+
+(defn spiral
+  ([size] (if (zero? size) '() (spiral size size 1)))
+  ([nrows ncols start]
+   (if (zero? nrows)
+     '(())
+     (let [first-row (list (range start (+ start ncols)))
+           inner-spiral (rotate (spiral ncols (dec nrows) (+ start ncols)))]
+       (concat first-row inner-spiral)))))
