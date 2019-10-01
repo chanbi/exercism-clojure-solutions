aa9d676360075e128d6eac27bfe18f0c34eaffa1
Chantal Bisson <2739403+chanbi@users.noreply.github.com>
2019-10-01 01:01:03 -0400

    solve complex-numbers exercise v2

--- complex-numbers/src/complex_numbers.clj
+++ complex-numbers/src/complex_numbers.clj
@@ -1,5 +1,14 @@
 (ns complex-numbers)
 
+(defn real [[a _]] a)
+
+(defn imaginary [[_ b]] b)
+
+(defn abs [[a b]]
+  (Math/sqrt (+ (* a a) (* b b))))
+
+(defn conjugate [[a b]] [a (- b)])
+
 (defn add [[a b] [c d]]
   [(+ a c) (+ b d)])
 
@@ -7,19 +16,8 @@
   [(- a c) (- b d)])
 
 (defn mul [[a b] [c d]]
-  [(- (* a c) (* b d))
-   (+ (* a d) (* b c))])
+  [(- (* a c) (* b d)) (+ (* a d) (* b c))])
 
 (defn div [[a b] [c d]]
-  (let [divisor (+ (* c c) (* d d))]
-    [(double (/ (+ (* a c) (* b d)) divisor))
-     (double (/ (- (* b c) (* a d)) divisor))]))
-
-(defn abs [[a b]]
-  (Math/sqrt (+ (* a a) (* b b))))
-
-(defn conjugate [[a b]] [a (- b)])
-
-(defn real [[a b]] a)
-
-(defn imaginary [[a b]] b)
+  [(double (/ (+ (* a c) (* b d)) (+ (* c c) (* d d))))
+   (double (/ (- (* b c) (* a d)) (+ (* c c) (* d d))))])
