86bb074a90ab02194f25bbf07d39f3d65a53d4f5
Chantal Bisson <2739403+chanbi@users.noreply.github.com>
2019-10-02 23:47:49 -0400

    solve rna-transcription exercise

--- rna-transcription/src/rna_transcription.clj
+++ rna-transcription/src/rna_transcription.clj
@@ -1,5 +1,14 @@
 (ns rna-transcription)
 
-(defn to-rna [dna] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn- transcribe [char]
+  (case char
+    \C \G
+    \G \C
+    \A \U
+    \T \A
+    (throw (AssertionError.))))
+
+(defn to-rna [dna]
+  {:pre [(string? dna)]
+   :post [(string? %)]}
+  (apply str (map transcribe dna)))
