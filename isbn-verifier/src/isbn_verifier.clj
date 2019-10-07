e905599c58ef571839b30a301235540ea7edc545
Chantal Bisson <2739403+chanbi@users.noreply.github.com>
2019-10-06 22:19:06 -0400

    solve isbn-verifier exercise v2

--- isbn-verifier/src/isbn_verifier.clj
+++ isbn-verifier/src/isbn_verifier.clj
@@ -2,18 +2,14 @@
 
 (def isbn-dgts {\0 0 \1 1 \2 2 \3 3 \4 4 \5 5 \6 6 \7 7 \8 8 \9 9 \X 10})
 
-(defn- filter-digits [isbn]
-  (filter isbn-dgts isbn))
+(defn- valid-pattern? [isbn]
+  (re-matches #"^(\d)-?(\d{3})-?(\d{5})-?([\dX])$" isbn))
 
-(defn- valid-digits? [isbn]
-  (let [digits (filter-digits isbn)]
-    (and (= 10 (count digits))
-         (every? (dissoc isbn-dgts \X) %) (butlast digits))))
+(defn- valid-formula? [numbers]
+  (zero? (mod (reduce + (map * (range 10 -1 -1) numbers)) 11)))
 
 (defn- to-numbers [isbn]
-  (map #(isbn-dgts %) isbn))
+  (map #(isbn-dgts %) (filter isbn-dgts isbn)))
 
 (defn isbn? [isbn]
-  (if (valid-digits? isbn)
-    (zero? (mod (reduce + (map-indexed #(* (inc %1) %2) (reverse (to-numbers (filter-digits isbn))))) 11))
-    false))
+  (and (valid-pattern? isbn) (valid-formula? (to-numbers isbn))))
